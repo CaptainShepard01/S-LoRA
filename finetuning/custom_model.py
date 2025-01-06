fb93cae00b89390a465d01d3f4c9c7b3d27b2e9b
Anton Balykov <49350081+CaptainShepard01@users.noreply.github.com>
2025-01-06 22:52:44 +0100

    Fixed training

--- finetuning/custom_model.py
+++ finetuning/custom_model.py
@@ -22,33 +22,37 @@ class Adapter(nn.Module, loralib.LoRALayer):
             lora_dropout: float = 0.,
             fan_in_fan_out: bool = False,
             # Set this to True if the layer to replace stores weight like (fan_in, fan_out)
-            merge_weights: bool = True,
+            merge_weights: bool = False,
             **kwargs
     ):
         nn.Module.__init__(self)
-        loralib.LoRALayer.__init__(self, r=r, lora_alpha=lora_alpha, lora_dropout=lora_dropout,
-                                   merge_weights=merge_weights)
-        self.fan_in_fan_out = fan_in_fan_out
-        self.weight = nn.Parameter()
+        loralib.LoRALayer.__init__(self, r=r, lora_alpha=lora_alpha, lora_dropout=lora_dropout, merge_weights=merge_weights)
+
+        # self.fan_in_fan_out = fan_in_fan_out
+
         # Actual trainable parameters
-        if r > 0:
-            self.lora_A = nn.Parameter(self.weight.new_zeros((r, in_features)))
-            self.lora_B = nn.Parameter(self.weight.new_zeros((out_features, r)))
-            self.scaling = self.lora_alpha / self.r
-            # Freezing the pre-trained weight matrix
-            self.weight.requires_grad = False
+        self.lora_A = nn.Parameter(torch.zeros((r, in_features)))
+        self.lora_B = nn.Parameter(torch.zeros((out_features, r)))
+
+        self.scaling = self.lora_alpha / self.r
+        # Freezing the pre-trained weight matrix
+        # weight.requires_grad = False
+
         self.reset_parameters()
 
     def reset_parameters(self):
-        if hasattr(self, 'lora_A'):
-            # initialize A the same way as the default for nn.Linear and B to zero
-            nn.init.kaiming_uniform_(self.lora_A, a=math.sqrt(5))
-            nn.init.zeros_(self.lora_B)
+        # initialize A the same way as the default for nn.Linear and B to zero
+        nn.init.kaiming_uniform_(self.lora_A, a=math.sqrt(5))
+        nn.init.zeros_(self.lora_B)
 
     def train(self, mode: bool = True):
         def T(w):
             return w.transpose(0, 1) if self.fan_in_fan_out else w
 
+        # if mode:
+        #     self.weight.data += T(self.lora_B @ self.lora_A) * self.scaling
+
+        # TODO: Check this
         # if mode:
         #     if self.merge_weights and self.merged:
         #         # Make sure that the weights are not merged
@@ -63,45 +67,54 @@ class Adapter(nn.Module, loralib.LoRALayer):
         #         self.merged = True
 
     def forward(self, x: torch.Tensor):
-        if self.r > 0 and not self.merged:
-            result = (self.lora_dropout(x) @ self.lora_A.transpose(0, 1) @ self.lora_B.transpose(0, 1)) * self.scaling
-            return result
-        else:
-            return None
+        result = (self.lora_dropout(x) @ self.lora_A.transpose(0, 1) @ self.lora_B.transpose(0, 1)) * self.scaling
+
+        # result = self.lora_A(x)
+        # result = self.lora_B(result)
+        # result = self.lora_dropout(result) * self.scaling
+
+        return result
 
 
 class MultiLinear(nn.Linear):
     """
     MultiLinear class to add multiple adapters to the BERT model. It has the original linear layer and multiple adapters.
     """
-    def __init__(self, linear, in_features, out_features, r, lora_alpha, lora_dropout, num_adapters=2,
-                 merge_weights=True, **kwargs):
+    def __init__(self, in_features, out_features, r, lora_alpha, lora_dropout=0.1, num_adapters=2,
+                 merge_weights=False, **kwargs):
         self.num_adapters = num_adapters
-        nn.Linear.__init__(self, in_features, out_features, **kwargs)
-        self.linear = linear
-        self.adapters = nn.ModuleList(Adapter(in_features, out_features, r, lora_alpha, lora_dropout, merge_weights) for _ in
-                                      range(num_adapters))
-        self.reset_parameters()
+        super().__init__(in_features, out_features, **kwargs)
+
+        self.adapters = nn.ModuleList(Adapter(in_features, out_features, r, lora_alpha, lora_dropout, merge_weights) for _ in range(num_adapters))
+
+        # self.reset_parameters()
 
     def reset_parameters(self):
-        nn.Linear.reset_parameters(self)
+        # nn.Linear.reset_parameters(self)
+        pass
 
-    def forward(self, x, masking):
-        result = self.linear(x)
+    def train(self, mode: bool = True):
+        nn.Linear.train(self, mode)
 
         for i in range(self.num_adapters):
-            if self.adapters[i].r > 0:
-                result += self.adapters[i](x) * masking[:, i].view(-1, 1, 1)
+            self.adapters[i].train(mode)
+
+    def forward(self, x, masking):
+        # result = self.linear(x)
+        result = F.linear(x, self.weight, bias=self.bias)
+
+        for i in range(self.num_adapters):
+            result += self.adapters[i](x) * masking[:, i].view(-1, 1, 1)
 
         return result
 
 
-class CustomBert(transformers.PreTrainedModel):
+class CustomBert(nn.Module):
     """
     Custom BERT model with LoRA layers applied to the query, key, and value layers of the self-attention mechanism.
     """
     def __init__(self, bert, num_adapters=2, rank=8, alpha=32):
-        super(CustomBert, self).__init__(config=BertConfig.from_pretrained('google/bert_uncased_L-2_H-128_A-2'))
+        super().__init__()
         self.bert = bert
         self.l1 = nn.Linear(128, 1)
         self.do = nn.Dropout(0.1)
@@ -113,35 +126,39 @@ class CustomBert(transformers.PreTrainedModel):
             if isinstance(module, nn.Linear) and "encoder" and "attention" in name:
                 idx = int(name.split(".")[2])
                 if "query" in name:
-                    self.bert.encoder.layer[idx].attention.self.query = MultiLinear(module,
-                                                                                    module.in_features,
-                                                                                    module.out_features, r=rank,
-                                                                                    lora_alpha=alpha, lora_dropout=0.1,
-                                                                                    num_adapters=num_adapters
-                                                                                    )
+                    new_layer = MultiLinear(module.in_features,
+                                            module.out_features,
+                                            r=rank,
+                                            lora_alpha=alpha, lora_dropout=0.1,
+                                            num_adapters=num_adapters
+                                            )
+                    new_layer.load_state_dict(module.state_dict(), strict=False)
+                    self.bert.encoder.layer[idx].attention.self.query = new_layer.to(module.weight.device)
 
-                    assert torch.allclose(module.weight,
-                                          self.bert.encoder.layer[idx].attention.self.query.linear.weight)
+                    assert torch.allclose(module.weight, self.bert.encoder.layer[idx].attention.self.query.weight)
 
                 elif "key" in name:
-                    self.bert.encoder.layer[idx].attention.self.key = MultiLinear(module,
-                                                                                  module.in_features,
-                                                                                  module.out_features, r=rank,
-                                                                                  lora_alpha=alpha, lora_dropout=0.1,
-                                                                                  num_adapters=num_adapters
-                                                                                  )
+                    new_layer = MultiLinear(module.in_features,
+                                            module.out_features,
+                                            r=rank,
+                                            lora_alpha=alpha, lora_dropout=0.1,
+                                            num_adapters=num_adapters
+                                            )
+                    new_layer.load_state_dict(module.state_dict(), strict=False)
+                    self.bert.encoder.layer[idx].attention.self.key = new_layer.to(module.weight.device)
 
-                    assert torch.allclose(module.weight, self.bert.encoder.layer[idx].attention.self.key.linear.weight)
+                    assert torch.allclose(module.weight, self.bert.encoder.layer[idx].attention.self.key.weight)
                 elif "value" in name:
-                    self.bert.encoder.layer[idx].attention.self.value = MultiLinear(module,
-                                                                                    module.in_features,
-                                                                                    module.out_features, r=rank,
-                                                                                    lora_alpha=alpha, lora_dropout=0.1,
-                                                                                    num_adapters=num_adapters
-                                                                                    )
+                    new_layer = MultiLinear(module.in_features,
+                                            module.out_features,
+                                            r=rank,
+                                            lora_alpha=alpha, lora_dropout=0.1,
+                                            num_adapters=num_adapters
+                                            )
+                    new_layer.load_state_dict(module.state_dict(), strict=False)
+                    self.bert.encoder.layer[idx].attention.self.value = new_layer.to(module.weight.device)
 
-                    assert torch.allclose(module.weight,
-                                          self.bert.encoder.layer[idx].attention.self.value.linear.weight)
+                    assert torch.allclose(module.weight, self.bert.encoder.layer[idx].attention.self.value.weight)
 
     def forward(self, x, mask, masking):
         bert_out = self.bert(x, attention_mask=mask, masking=masking)
